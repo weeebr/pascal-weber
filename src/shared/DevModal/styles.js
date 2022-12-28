c4c7f5755a541ac50f35feb4150fc9c68efc7bbd
Pascal Weber <p.weber.92@gmail.com>
2022-12-28 14:57:35 -0500

    that must be it

--- src/shared/DevModal/styles.js
+++ src/shared/DevModal/styles.js
@@ -140,6 +140,7 @@ export const ModalContent = styled.div`
     width: 100%;
     flex-grow: 1;
     overflow: hidden;
+    height: calc(100vh - 230px);
 
     .description {
       font-size: 18px;
