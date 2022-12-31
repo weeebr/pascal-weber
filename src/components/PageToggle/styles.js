0b04136b216b66c52c23da4cdbf3700318e863cf
Pascal Weber <p.weber.92@gmail.com>
2022-12-31 13:09:49 -0500

    fix warnings

--- src/components/PageToggle/styles.js
+++ src/components/PageToggle/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { theme, themeDark } from 'shared/theme';
+import { theme } from 'shared/theme';
 
 export const StyledButton = styled.button`
   border: none;
--- src/components/ThemeToggle/index.js
+++ src/components/ThemeToggle/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { SunIcon, MoonIcon } from "shared/icons";
 import "./styles.css";
 
